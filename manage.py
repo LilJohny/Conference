1a838bedecc7a61bcd9a86ef544789ff50347843
Denis <mister.denis67808@gmail.com>
2019-07-11 23:44:47 +0300

    Refactoring and renaming

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Conference.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Conferences.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
